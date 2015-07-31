6659bb949668c0ad0993e03c92d12511de4d56ce
GouthamR <gouthamrajeev1+github@gmail.com>
2015-07-30 21:13:08 -0700

    Implemented operator overloads on Time. Days' overlapsWith includes equals case. Added printUnitTest method. Added ClassTime unit tests.

--- ScheduleCreator.py
+++ ScheduleCreator.py
@@ -12,6 +12,16 @@ class Time:
         self.minute = int(rawMinute[:2]) #cuts off p if necessary
     def __str__(self):
         return "Time: %s:%s" % (self.hour, self.minute)
+    def __eq__(self, other):
+        return self.hour == other.hour and self.minute == other.minute
+    def __le__(self, other):
+        return (self == other #same hour and same minute
+            or ((self.hour == other.hour) and (self.minute < other.minute)) #same hour, different minute
+            or self.hour < other.hour) #different hour and different minute
+    def __ge__(self, other):
+        return (self == other #same hour and same minute
+            or ((self.hour == other.hour) and (self.minute > other.minute)) #same hour, different minute
+            or self.hour > other.hour) #different hour and different minute
 
 class Days:
     def __init__(self, rawData):
@@ -29,8 +39,8 @@ class ClassTime:
         self.end = Time(timeStrList[1])
     def __str__(self):
         return "ClassTime: %s, %s" % (self.start, self.end)
-    def overlapsWith(otherTime):
-        return self.start < otherTime.end or self.end > otherTime.start
+    def overlapsWith(self, otherTime):
+        return self.start <= otherTime.end or self.end >= otherTime.start
 
 class Class:
     DATA_TIMINGS_INDEX = 5
@@ -43,9 +53,26 @@ class Class:
     def __str__(self):
         return "Class: %s, %s" % (self.days, self.classTime)
 
+def printUnitTest(testName, *testResults): #testResult = True is success, False is failure
+    print("%s: %s" % (testName, testResults))
+
 def unitTests():
     classRawStr = "44215	Lec	A	4	STAFF	MWF   8:00- 8:50	DBH 1100	Sat, Dec 5, 1:30-3:30pm	221	34	0	51	111	A and N	Bookstore	 	OPEN"
-    print("Class parse test: %s" % (str(Class(classRawStr)) == "Class: Days: [0, 2, 4], ClassTime: Time: 8:0, Time: 8:50"))
+    printUnitTest("Class parse test", str(Class(classRawStr)) == "Class: Days: [0, 2, 4], ClassTime: Time: 8:0, Time: 8:50")
+##    classTimeRawStr1 = "MWF   8:00- 8:50" #overlaps with self
+##    classTimeRawStr2 = "MWF   8:30- 9:00" #overlaps with 1
+##    classTimeRawStr3 = "MWF   7:30- 8:30" #does overlap with 1
+##    classTimeRawStr4 = "TuTh   7:30- 8:30" #does not overlap with 1
+    classTimeRawStr1 = "8:00- 8:50" #overlaps with self
+    classTimeRawStr2 = "8:30- 9:00" #overlaps with end of 1
+    classTimeRawStr3 = "7:30- 8:30" #overlaps with beginning of 1
+    classTimeRawStr4 = "7:30- 7:50" #does not overlap with 1 - before beginning
+    classTimeRawStr5 = "9:00- 9:30" #does not overlap with 1 - after end
+    printUnitTest("ClassTime overlap tests", ClassTime(classTimeRawStr1).overlapsWith(ClassTime(classTimeRawStr1)),
+                  ClassTime(classTimeRawStr1).overlapsWith(ClassTime(classTimeRawStr2)),
+                  ClassTime(classTimeRawStr1).overlapsWith(ClassTime(classTimeRawStr3)),
+                  not ClassTime(classTimeRawStr1).overlapsWith(ClassTime(classTimeRawStr4)),
+                  not ClassTime(classTimeRawStr1).overlapsWith(ClassTime(classTimeRawStr5)))
     print("End unit tests.")
 
 def main():
@@ -61,4 +88,4 @@ def main():
     print ("%s" % "\n".join(map(str, classes)))
 
 unitTests()
-main()
+#main()
