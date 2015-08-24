e0c7ca336e7a1249921b8a5f4ee32c2b7397c2bb
Goutham R <gouthamrajeev1+git@gmail.com>
2015-08-24 13:58:37 -0700

    To generateScheduleUnitTests, add unit test for overlapping classes.

--- ScheduleCreator.py
+++ ScheduleCreator.py
@@ -159,9 +159,12 @@ def generateScheduleUnitTests():
     course3.addClass(Class("32000	test	test	test	test	MW   7:00- 8:00	testinga	test	test	test	test	test	test	test	test"))
     course4 = Course("TestCourse 4A")
     course4.addClass(Class("41000	test	test	test	test	TuTh   10:00- 11:00	testinga	test	test	test	test	test	test	test	test"))
+    course5 = Course("TestCourse 5A")
+    course5.addClass(Class("51000	test	test	test	test	TuTh   10:00- 10:50	testinga	test	test	test	test	test	test	test	test"))
     printUnitTest("Generate schedule unit tests", [i.getClassCodes() for i in generatePossibleSchedules([course2])] == [[21000], [22000]],
                   [i.getClassCodes() for i in generatePossibleSchedules([course2, course4])] == [[21000, 41000], [22000, 41000]],
-                  [i.getClassCodes() for i in generatePossibleSchedules([course2, course3])] == [[21000, 31000], [21000, 32000], [22000, 31000], [22000, 32000]])
+                  [i.getClassCodes() for i in generatePossibleSchedules([course2, course3])] == [[21000, 31000], [21000, 32000], [22000, 31000], [22000, 32000]],
+                  len(generatePossibleSchedules([course4, course5])) == 0)
 
 def unitTests():
     classRawStr = "44215	Lec	A	4	STAFF	MWF   8:00- 8:50	DBH 1100	Sat, Dec 5, 1:30-3:30pm	221	34	0	51	111	A and N	Bookstore	 	OPEN"
