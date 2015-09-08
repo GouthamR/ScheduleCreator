e4808df92c732688ea814b598f4ccef55529175f
Goutham R <gouthamrajeev1+git@gmail.com>
2015-09-08 13:11:00 -0700

    Move input methods into ScheduleInput module.

--- ScheduleCreator.py
+++ ScheduleCreator.py
@@ -5,6 +5,7 @@
 
 from Course import *
 from Schedule import *
+from ScheduleInput import *
 
 def printUnitTest(testName, *testResults): #testResult = True is success, False is failure
     for i in testResults:
@@ -128,78 +129,6 @@ def unitTests():
     redZoneUnitTests()
     classTimeAMPMUnitTests()
     print("End unit tests.")
-
-#Removes newline from end of inputStr if necessary and returns it.
-#Does NOT modify inputStr
-def removeNewline(inputStr):
-    if inputStr.endswith("\n"):
-        return inputStr[:-1] #remove newline
-    #else:
-    return inputStr
-
-#Returns next line, removing newline from end if necessary
-def readNextLine(file):
-    return removeNewline(file.readline())
-
-def fileInputClasses(file, course):
-    currInput = "_flag_"
-    while currInput != "":
-        currInput = readNextLine(file)
-        if(currInput != ""):
-            course.addClass(Class(currInput))
-
-CONNECTED_COURSE_INDICATOR = "_C_"
-
-def readStandardCourses(file, courses, currInput):
-    courses.append(Course(currInput))
-    fileInputClasses(file, courses[-1])
-
-def readConnectedCourses(file, courses, connectedClassDict, firstLine):
-    lectureCourse, labCourse = (Course(i) for i in firstLine.split(CONNECTED_COURSE_INDICATOR))
-
-    isLecture = False
-    lastType = None
-    currLine = "_flag_"
-    key = None
-
-    while currLine != "":
-        currLine = readNextLine(file)
-        if currLine != "":
-            currClass = Class(currLine)
-            if currClass.type != lastType:
-                isLecture = not isLecture
-            if isLecture:
-                lectureCourse.addClass(currClass)
-                key = currClass.code
-                connectedClassDict[key] = []
-            else:
-                labCourse.addClass(currClass)
-                connectedClassDict[key].append(currClass)
-            lastType = currClass.type
-    
-    courses.append(lectureCourse)
-    courses.append(labCourse)
-
-def fileInputCourses(fileName):
-    courses = []
-    connectedClassDict = {}
-    with open(fileName, 'r') as file:
-        currInput = "_flag_"
-        while currInput != "":
-            currInput = readNextLine(file)
-            if(currInput != ""):
-                if CONNECTED_COURSE_INDICATOR in currInput:
-                    readConnectedCourses(file, courses, connectedClassDict, currInput)
-                else:
-                    readStandardCourses(file, courses, currInput)
-    return courses, connectedClassDict
-
-def fileInputRedZones(fileName):
-    redZones = []
-    with open(fileName, 'r') as file:
-        for line in file:
-            redZones.append(ClassTime(removeNewline(line)))
-    return redZones
 
 def main():
     courses, connectedClassDict = fileInputCourses("actual_input_3.txt")
---
+++ ScheduleInput.py
@@ -0,0 +1,73 @@
+from Course import *
+
+#Removes newline from end of inputStr if necessary and returns it.
+#Does NOT modify inputStr
+def removeNewline(inputStr):
+    if inputStr.endswith("\n"):
+        return inputStr[:-1] #remove newline
+    #else:
+    return inputStr
+
+#Returns next line, removing newline from end if necessary
+def readNextLine(file):
+    return removeNewline(file.readline())
+
+def fileInputClasses(file, course):
+    currInput = "_flag_"
+    while currInput != "":
+        currInput = readNextLine(file)
+        if(currInput != ""):
+            course.addClass(Class(currInput))
+
+CONNECTED_COURSE_INDICATOR = "_C_"
+
+def readStandardCourses(file, courses, currInput):
+    courses.append(Course(currInput))
+    fileInputClasses(file, courses[-1])
+
+def readConnectedCourses(file, courses, connectedClassDict, firstLine):
+    lectureCourse, labCourse = (Course(i) for i in firstLine.split(CONNECTED_COURSE_INDICATOR))
+
+    isLecture = False
+    lastType = None
+    currLine = "_flag_"
+    key = None
+
+    while currLine != "":
+        currLine = readNextLine(file)
+        if currLine != "":
+            currClass = Class(currLine)
+            if currClass.type != lastType:
+                isLecture = not isLecture
+            if isLecture:
+                lectureCourse.addClass(currClass)
+                key = currClass.code
+                connectedClassDict[key] = []
+            else:
+                labCourse.addClass(currClass)
+                connectedClassDict[key].append(currClass)
+            lastType = currClass.type
+    
+    courses.append(lectureCourse)
+    courses.append(labCourse)
+
+def fileInputCourses(fileName):
+    courses = []
+    connectedClassDict = {}
+    with open(fileName, 'r') as file:
+        currInput = "_flag_"
+        while currInput != "":
+            currInput = readNextLine(file)
+            if(currInput != ""):
+                if CONNECTED_COURSE_INDICATOR in currInput:
+                    readConnectedCourses(file, courses, connectedClassDict, currInput)
+                else:
+                    readStandardCourses(file, courses, currInput)
+    return courses, connectedClassDict
+
+def fileInputRedZones(fileName):
+    redZones = []
+    with open(fileName, 'r') as file:
+        for line in file:
+            redZones.append(ClassTime(removeNewline(line)))
+    return redZones
