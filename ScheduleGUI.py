f59b6a9824656fcdc24a02936d16a84644c9c9c8
Goutham R <gouthamrajeev1+git@gmail.com>
2015-09-09 13:37:04 -0700

    Continue writing ScheduleGUI.

--- ScheduleGUI.py
+++ ScheduleGUI.py
@@ -1,9 +1,12 @@
 from tkinter import *
+from Schedule import *
+from Course import *
 
 class ScheduleGUI:
     def switchSchedule(self):
         self.scheduleIndex += 1
         print("switch to " + str(self.scheduleIndex))
+        drawSchedule()
     def initGUI(self):
         root = Tk()
         root.protocol('WM_DELETE_WINDOW', root.destroy)
@@ -17,14 +20,19 @@ class ScheduleGUI:
         width = 400
         height = 400
         # invoke canvas
-        c = Canvas(root, width=width, height=height)
-        c.pack()
-        c.create_text(10, 20, anchor=SW, text="Schedule")
-        c.create_rectangle(50, 25, 150, 75, fill="blue")
+        self.canvas = Canvas(root, width=width, height=height)
+        self.canvas.pack()
+    def drawSchedule(self):
+        self.canvas.create_text(10, 20, anchor=SW, text="Schedule")
+        self.canvas.create_rectangle(50, 25, 150, 75, fill="blue")
+        for i in range(len(self.schedules[self.scheduleIndex].classes)):
+            currClass = self.schedules[self.scheduleIndex].classes[i]
+            self.canvas.create_text(i*10, i*20, anchor=SW, text=currClass.code)
     def __init__(self, schedules):
         self.schedules = schedules
         self.scheduleIndex = 0
         self.initGUI()
+        self.drawSchedule()
         mainloop()
 
 if __name__ == "__main__":
