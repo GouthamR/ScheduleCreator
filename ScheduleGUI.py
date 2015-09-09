c88a1ed812154056a5f6e4eb2c68e47fb3f8d016
Goutham R <gouthamrajeev1+git@gmail.com>
2015-09-09 13:27:27 -0700

    Continue writing ScheduleGUI.

--- ScheduleGUI.py
+++ ScheduleGUI.py
@@ -1,14 +1,17 @@
 from tkinter import *
 
 class ScheduleGUI:
-    def __init__(self, schedules):
+    def switchSchedule(self):
+        self.scheduleIndex += 1
+        print("switch to " + str(self.scheduleIndex))
+    def initGUI(self):
         root = Tk()
+        root.protocol('WM_DELETE_WINDOW', root.destroy)
         root.title("Schedule Creator - Schedule Viewer. By Goutham Rajeev")
-        self.schedules = schedules
-        # create frame to put control buttons onto
+        # create frame
         frame = Frame(root, bg="grey", width=400, height=40)
         frame.pack(fill='x')
-        button1 = Button(frame, text="Switch")
+        button1 = Button(frame, text="Switch", command=self.switchSchedule)
         button1.pack(side="right", padx=10)
         # set canvas properties
         width = 400
@@ -18,3 +21,11 @@ class ScheduleGUI:
         c.pack()
         c.create_text(10, 20, anchor=SW, text="Schedule")
         c.create_rectangle(50, 25, 150, 75, fill="blue")
+    def __init__(self, schedules):
+        self.schedules = schedules
+        self.scheduleIndex = 0
+        self.initGUI()
+        mainloop()
+
+if __name__ == "__main__":
+    ScheduleGUI(None)
