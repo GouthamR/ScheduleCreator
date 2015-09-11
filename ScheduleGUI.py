6fd860a6a4c40fbc933e2d1ef0a709b344aaf3cf
Goutham R <gouthamrajeev1+git@gmail.com>
2015-09-11 12:24:30 -0700

    Continue writing ScheduleGUI. Assumes Time has getMinutes method, which has not been written yet.

--- ScheduleGUI.py
+++ ScheduleGUI.py
@@ -3,6 +3,12 @@ from Schedule import *
 from Course import *
 
 class ScheduleGUI:
+    CANVAS_WIDTH = 400
+    CANVAS_HEIGHT = 400
+    NUMBER_OF_DAYS = 5
+    MINUTES_IN_DAY = 1440
+    BLOCK_WIDTH = CANVAS_WIDTH/NUMBER_OF_DAYS
+    BLOCK_COLORS = ("white", "red", "green", "blue", "cyan", "yellow", "magenta")
     def switchSchedule(self):
         self.scheduleIndex += 1
         print("switch to " + str(self.scheduleIndex))
@@ -16,18 +22,25 @@ class ScheduleGUI:
         frame.pack(fill='x')
         button1 = Button(frame, text="Switch", command=self.switchSchedule)
         button1.pack(side="right", padx=10)
-        # set canvas properties
-        width = 400
-        height = 400
         # invoke canvas
-        self.canvas = Canvas(root, width=width, height=height)
+        self.canvas = Canvas(root, width=ScheduleGUI.CANVAS_WIDTH, height=ScheduleGUI.CANVAS_HEIGHT)
         self.canvas.pack()
+    def getCanvasY(minutes):
+        return minutes / ScheduleGUI.MINUTES_IN_DAY * ScheduleGUI.CANVAS_HEIGHT
     def drawSchedule(self):
-        self.canvas.create_text(10, 20, anchor=SW, text="Schedule")
-        self.canvas.create_rectangle(50, 25, 150, 75, fill="blue")
-        for i in range(len(self.schedules[self.scheduleIndex].classes)):
-            currClass = self.schedules[self.scheduleIndex].classes[i]
-            self.canvas.create_text(i*10, i*20, anchor=SW, text=currClass.code)
+        self.canvas.delete("all")
+        classes = self.schedules[self.scheduleIndex].classes
+        for i in range(len(classes)):
+            currClass = classes[i]
+            currColor = ScheduleGUI.BLOCK_COLORS[i]
+            for day in currClass.days.days:
+                startMin = currClass.classTime.start.getMinutes()
+                endMin = currClass.classTime.end.getMinutes()
+                x = day * ScheduleGUI.BLOCK_WIDTH
+                y = ScheduleGUI.getCanvasY(startMin)
+                w = ScheduleGUI.BLOCK_WIDTH
+                h = ScheduleGUI.getCanvasY(endMin - startMin)
+                self.canvas.create_rectangle(x, y, x + w, y + h, fill=currColor)
     def __init__(self, schedules):
         self.schedules = schedules
         self.scheduleIndex = 0
