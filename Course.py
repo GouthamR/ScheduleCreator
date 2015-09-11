93d78bc881115833a14d357eb1d493c63829cfa9
Goutham R <gouthamrajeev1+git@gmail.com>
2015-09-11 12:27:18 -0700

    Create Time getMinutes method.

--- Course.py
+++ Course.py
@@ -15,6 +15,8 @@ class Time:
         rawHour, rawMinute = rawData.split(":")
         self.hour = Time.getHourFromInput(rawHour, rawMinute.endswith("p"))
         self.minute = int(rawMinute[:2]) #cuts off p if necessary
+    def getMinutes(self):
+        return (self.hour * 60 + self.minute)
     def __str__(self):
         return "Time: %s:%s" % (self.hour, self.minute)
     def __lt__(self, other):
