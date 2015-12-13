98ce1c23595df67ca1922468420b51c33d355180
Goutham R <gouthamrajeev1+git@gmail.com>
2015-12-12 17:21:15 -0800

    Rename private functions according to underscore convention.

--- PlaintextParser.py
+++ PlaintextParser.py
@@ -7,35 +7,35 @@ FILE_NAME = "ics32.txt"
 COURSE_NAME = FILE_NAME.replace(".txt", "")
 OUT_FILE_NAME = "output.txt"
 
-def isInt(input: str) -> int:
+def _isInt(input: str) -> int:
 	""" Checks if input str contains an integer. """
 	try:
 		int(input)
 		return True
 	except Exception:
 		return False
-assert isInt('534')
-assert isInt('-534')
-assert isInt('0')
-assert isInt('-0')
-assert not isInt('a0')
-assert not isInt('0a')
-assert not isInt('asdf')
+assert _isInt('534')
+assert _isInt('-534')
+assert _isInt('0')
+assert _isInt('-0')
+assert not _isInt('a0')
+assert not _isInt('0a')
+assert not _isInt('asdf')
 
-def isTableHeader(line: str) -> bool:
+def _isTableHeader(line: str) -> bool:
 	"""
 	Returns True if line is in format of course table header, with column
 	names (as in COLUMN_NAMES).
 	"""
 	return line.strip().startswith(COLUMN_NAMES[0])
 
-def isClassFormat(line: str) -> bool:
+def _isClassFormat(line: str) -> bool:
 	"""
 	Returns True if line is in format of class fields.
 	"""
-	return isInt(line.strip()[0:5])
+	return _isInt(line.strip()[0:5])
 
-def getColumnIndices(line: str) -> 'list of (tuple of int)':
+def _getColumnIndices(line: str) -> 'list of (tuple of int)':
 	"""
 	Returns list representing position of each column, from COLUMN_NAMES, in line.
 	Each element is a tuple of form (start index, end index), except for
@@ -47,7 +47,7 @@ def getColumnIndices(line: str) -> 'list of (tuple of int)':
 	column_indices.append( (line.index(COLUMN_NAMES[-1]), None) )
 	return column_indices
 
-def getClass(line: str, column_indices: 'list of (tuple of int)') -> 'tuple of str':
+def _getClass(line: str, column_indices: 'list of (tuple of int)') -> 'tuple of str':
 	"""
 	Returns tuple of class fields corresponding to input line,
 	using table format based on column_indices.
@@ -67,13 +67,13 @@ def readCourseFileToTuples(fileName: str) -> 'list of (tuple of str)':
 	curr_column_indices = None
 	with open(fileName, 'r') as f:
 		for line in f:
-			if isTableHeader(line):
-				curr_column_indices = getColumnIndices(line)
-			elif isClassFormat(line):
-				tuples.append(getClass(line, curr_column_indices))
+			if _isTableHeader(line):
+				curr_column_indices = _getColumnIndices(line)
+			elif _isClassFormat(line):
+				tuples.append(_getClass(line, curr_column_indices))
 	return tuples
 
-def isConnected(class_tuples: 'list of tuple') -> bool:
+def _isConnected(class_tuples: 'list of tuple') -> bool:
 	"""
 	Returns if course represented by argument is a connected course.
 	"""
@@ -84,31 +84,31 @@ def isConnected(class_tuples: 'list of tuple') -> bool:
 def _isConnectedAssertions():
 	lec = ('', 'LEC ', '', '', '', '', '', '', '', '', '', '', '', '', '')
 	lab = ('', 'LAB ', '', '', '', '', '', '', '', '', '', '', '', '', '')
-	assert not isConnected( () )
-	assert not isConnected( (lec, ) )
-	assert isConnected( (lec, lab) )
-	assert not isConnected( (lec, lec) )
-	assert isConnected( (lec, lab, lec) )
-	assert not isConnected( (lec, lec, lab, lec) )
+	assert not _isConnected( () )
+	assert not _isConnected( (lec, ) )
+	assert _isConnected( (lec, lab) )
+	assert not _isConnected( (lec, lec) )
+	assert _isConnected( (lec, lab, lec) )
+	assert not _isConnected( (lec, lec, lab, lec) )
 _isConnectedAssertions()
 
-def getType1Name(tuples: 'list of tuple') -> str:
+def _getType1Name(tuples: 'list of tuple') -> str:
 	"""
 	Returns name of type of first class in course.
 	"""
 	return tuples[0][TYPE_INDEX]
-def getType2Name(tuples: 'list of tuple') -> str:
+def _getType2Name(tuples: 'list of tuple') -> str:
 	"""
 	Returns name of second type for connected course.
 	Assumes course is connected.
 	"""
-	type1 = getType1Name(tuples)
+	type1 = _getType1Name(tuples)
 	for tup in tuples:
 		currType = tup[TYPE_INDEX]
 		if currType != type1:
 			return currType
 
-def getSubCourses(tuples: 'list of tuple') -> 'list of list of tuple':
+def _getSubCourses(tuples: 'list of tuple') -> 'list of list of tuple':
 	"""
 	Splits argument course into multiple sub-courses, where each sub-course
 	is made up of the classes of one type.
@@ -126,12 +126,12 @@ def getSubCourses(tuples: 'list of tuple') -> 'list of list of tuple':
 def _getSubCoursesAssertions():
 	lec = ('', 'LEC ', '', '', '', '', '', '', '', '', '', '', '', '', '')
 	lab = ('', 'LAB ', '', '', '', '', '', '', '', '', '', '', '', '', '')
-	assert getSubCourses([]) == []
-	assert getSubCourses([lec]) == [[lec]]
-	assert getSubCourses([lec, lec]) == [[lec, lec]]
-	assert getSubCourses([lec, lab]) == [[lec], [lab]]
-	assert getSubCourses([lec, lec, lab, lab]) == [[lec, lec], [lab, lab]]
-	assert getSubCourses([lec, lab, lec, lab]) == [[lec], [lab], [lec], [lab]]
+	assert _getSubCourses([]) == []
+	assert _getSubCourses([lec]) == [[lec]]
+	assert _getSubCourses([lec, lec]) == [[lec, lec]]
+	assert _getSubCourses([lec, lab]) == [[lec], [lab]]
+	assert _getSubCourses([lec, lec, lab, lab]) == [[lec, lec], [lab, lab]]
+	assert _getSubCourses([lec, lab, lec, lab]) == [[lec], [lab], [lec], [lab]]
 _getSubCoursesAssertions()
 
 def outputTuplesToFile(tuples: 'list of tuple', courseName: str) -> None:
@@ -139,22 +139,22 @@ def outputTuplesToFile(tuples: 'list of tuple', courseName: str) -> None:
 	Outputs argument to file.
 	"""
 	# with open(OUT_FILE_NAME, 'w') as f:
-	# 	if isConnected(tuples):
-	# 		f.write("{0} {1}_C_{0} {2}\n".format(courseName, getType1Name(tuples), getType2Name(tuples)))
+	# 	if _isConnected(tuples):
+	# 		f.write("{0} {1}_C_{0} {2}\n".format(courseName, _getType1Name(tuples), _getType2Name(tuples)))
 	# 		for tup in tuples:
 	# 			f.write(str(tup) + '\n')
 	# 	else:
-	# 		for subCourse in getSubCourses(tuples):
-	# 			f.write("{0} {1}\n".format(courseName, getType1Name(subCourse)))
+	# 		for subCourse in _getSubCourses(tuples):
+	# 			f.write("{0} {1}\n".format(courseName, _getType1Name(subCourse)))
 	# 			for tup in subCourse:
 	# 				f.write(str(tup) + '\n')
 	with open(OUT_FILE_NAME, 'w') as f:
-		if isConnected(tuples):
+		if _isConnected(tuples):
 			f.write("Connected:\n")
 			f.write("{}\n\n".format(Course(courseName, tuples)))
 		else:
 			f.write("Not Connected:\n")
-			for subCourse in getSubCourses(tuples):
+			for subCourse in _getSubCourses(tuples):
 				f.write("{}\n".format(Course(courseName, subCourse)))
 			f.write("\n")
 
