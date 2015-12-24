d16d772cb9969d7a846eca183714410b29dafc2d
Goutham R <gouthamrajeev1+git@gmail.com>
2015-12-24 15:12:45 -0800

    Add test_getCoursesParamsFromFile and associated unit test files. Tests do not pass.

--- UnitTests.py
+++ UnitTests.py
@@ -3,6 +3,7 @@ from Course import *
 from Schedule import *
 from ScheduleInput import *
 from ScheduleInput import _isInt, _isConnected, _convertToClassesByType
+from WebsiteInput import *
 from WebsiteInput import _getFileName, Term, _getTerm, _getDept, _getCourseName, _getCourseCodes
 class ClassTests(unittest.TestCase):
 
@@ -372,6 +373,20 @@ class WebsiteInputTests(unittest.TestCase):
         self.assertRaises(ValueError, _getCourseCodes, "I&C SCI\nICS 32\n36600-36623\nasdf")
         self.assertRaises(ValueError, _getCourseCodes, "I&C SCI\nICS 32\n36600-36623\n   ")
         self.assertRaises(ValueError, _getCourseCodes, "I&C SCI\nICS 32\n36600-36623\n")
+
+    def test_getCoursesParamsFromFile(self):
+        """
+        getCoursesParamsFromFile should return correct values.
+        Particularly tests if returns correct course codes.
+        """
+        firstParams = (Term.WINTER, 2016, ["I&C SCI", "HUMAN", "I&C SCI"], ["ICS 32", "HUMAN 1B", "ICS 6B"])
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_1.txt"), firstParams + (["36600-36623", "28100-28126", "49100-49130"], ))
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_2.txt"), firstParams + (["", "28100-28126", "49100-49130"], ))
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_3.txt"), firstParams + (["", "28100-28126", "49100-49130"], ))
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_4.txt"), firstParams + (["", "28100-28126", ""], ))
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_5.txt"), firstParams + (["", "28100-28126", ""], ))
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_6.txt"), firstParams + (["", "", ""], ))
+        self.assertEqual(getCoursesParamsFromFile("unit_test_web_input_7.txt"), firstParams + (["", "", ""], ))
 
 if __name__ == "__main__":
     unittest.main()
