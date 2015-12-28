a5ba620b14e0241b227844599cc47074324242ae
Goutham R <gouthamrajeev1+git@gmail.com>
2015-12-27 16:02:13 -0800

    Incorporate WebsiteInput into ScheduleCreatorMain. Unit tests pass and program works.

--- ScheduleCreatorMain.py
+++ ScheduleCreatorMain.py
@@ -8,15 +8,17 @@ from Course import *
 from Schedule import *
 from ScheduleInput import *
 from ScheduleGUI import *
+from WebsiteInput import *
 
-INPUT_FILE_NAME = "input.txt"
+INPUT_FILE_NAME = "web_input.txt"
 REDZONE_FILE_NAME = "red_zones.txt"
 MINUTESBETWEEN_FILE_NAME = "minutes_between_classes.txt"
 
 def main():
     print("CREATED BY GOUTHAM RAJEEV")
     print("Copyright 2016 Goutham Rajeev.  All rights reserved.")
-    courses, connectedClassDict = fileInputCourses(INPUT_FILE_NAME)
+    courseFileNames = writeCoursesWebDataToFiles(*getCoursesParamsFromFile(INPUT_FILE_NAME))
+    courses, connectedClassDict = fileInputCourses(courseFileNames)
     print("Starting schedule generation...")
     schedules = generatePossibleSchedules(courses, connectedClassDict)
     redZones = fileInputRedZones(REDZONE_FILE_NAME)
