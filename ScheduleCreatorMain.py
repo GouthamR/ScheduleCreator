04872056e17ba90a30a0c97a8db2e4105faf3f5e
GouthamR <gouthamrajeev1+github@gmail.com>
2017-05-13 17:58:30 -0700

    Refactor WebsiteInput module public functions into WebsiteInput class.

--- ScheduleCreatorMain.py
+++ ScheduleCreatorMain.py
@@ -13,16 +13,18 @@ from WebsiteInput import *
 INPUT_FILE_NAME = "config/web_input.txt"
 REDZONE_FILE_NAME = "config/red_zones.txt"
 MINUTESBETWEEN_FILE_NAME = "config/minutes_between_classes.txt"
+COURSEFILENAMES_FILE_NAME = "config/coursefilenames.txt"
 
 def runProgram():
     print("CREATED BY GOUTHAM RAJEEV")
     print("Copyright 2016 Goutham Rajeev.  All rights reserved.")
-    if savedCourseFileExists():
+    websiteInput = WebsiteInput(INPUT_FILE_NAME, COURSEFILENAMES_FILE_NAME)
+    if websiteInput.courseFilenamesConfigFileExists():
         print("Loading from saved course file...")
     else:
         print("Loading from website...")
-        scrapeCoursesDataFromWebsiteAndSaveToFiles(*parseCoursesParamsFromConfigFile(INPUT_FILE_NAME))
-    courseFileNames = readSavedCourseFileNames()
+        websiteInput.scrapeCoursesDataFromWebsiteAndSaveToFiles()
+    courseFileNames = websiteInput.readCourseFilenamesFromConfigFile()
     courses, connectedClassDict = fileInputCourses(courseFileNames)
     print("Starting schedule generation...")
     schedules = generatePossibleSchedules(courses, connectedClassDict)
