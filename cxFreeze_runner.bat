f83f42c57b417ba4862eac7434c01a3a839d708c
GouthamR <gouthamrajeev1+github@gmail.com>
2017-09-16 15:02:43 -0700

    Update build runners to copy coursefiles directory.

--- cxFreeze_runner.bat
+++ cxFreeze_runner.bat
@@ -1,3 +1,4 @@
 rmdir /s /q dist
 python cxFreezeSetup.py build
 robocopy config\ dist\config /s /e
+robocopy coursefiles\ dist\coursefiles /s /e
--- py2exe_runner.bat
+++ py2exe_runner.bat
@@ -1,3 +1,4 @@
 rmdir /s /q dist
 py -m py2exe.build_exe ScheduleCreatorMain.py
 robocopy config\ dist\config /s /e
+robocopy coursefiles\ dist\coursefiles /s /e
